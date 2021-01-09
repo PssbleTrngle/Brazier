15dd2833faaaeacaf1cac388ff15955827d09911
PssbleTrngle <nik@somethingcatchy.net>
2021-01-09 23:16:49 +0100

    Update link

---
+++ common/src/link.bat
@@ -0,0 +1,6 @@
+mklink /J "./main/resources/data/brazier/recipes" "./generated/resources/data/brazier/recipes"
+mklink /J "./main/resources/data/brazier/loot_tables" "./generated/resources/data/brazier/loot_tables"
+mklink /J "./main/resources/data/brazier/advancements" "./generated/resources/data/brazier/advancements"
+
+mklink /J "./main/resources/assets/brazier/models/item" "./generated/resources/assets/brazier/models/item"
+mklink /J "./main/resources/assets/brazier/blockstates" "./generated/resources/assets/brazier/blockstates"
